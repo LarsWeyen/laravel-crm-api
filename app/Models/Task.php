2b66f85e62231fce906d537883cad90853d6d1e8
LarsWeyen <97236276+LarsWeyen@users.noreply.github.com>
2024-02-19 13:50:39 +0100

    feat: add task model and migration

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,31 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'user_id',
+        'client_id',
+        'project_id',
+        'deadline',
+        'status'
+    ];
+
+    public const STATUS = ['open', 'in progress', 'pending', 'waiting client', 'blocked', 'closed'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
 }
