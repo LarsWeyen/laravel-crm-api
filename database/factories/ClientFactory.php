e2001d4d2081cd1f5b096b9a01f7a9601c355c54
LarsWeyen <97236276+LarsWeyen@users.noreply.github.com>
2024-02-19 16:36:26 +0100

    feat: define default state of clientfactory

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -16,8 +16,16 @@ class ClientFactory extends Factory
      */
     public function definition(): array
     {
+        
         return [
-            //
+            'contact_name' => $this->faker->name(),
+            'contact_email' =>  $this->faker->email(),
+            'contact_phone_number' => $this->faker->e164PhoneNumber(),
+            'company_name' => $this->faker->company(),
+            'company_address' => $this->faker->streetAddress(),
+            'company_city' => $this->faker->city(),
+            'company_zip' => $this->faker->postCode(),
+            'company_vat' => $this->faker->numberBetween(1000,100000)
         ];
     }
 }
