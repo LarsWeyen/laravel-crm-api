0ae1090299f9ff0d5b78078f46eba113bbcf65fb
LarsWeyen <97236276+LarsWeyen@users.noreply.github.com>
2024-02-19 13:50:22 +0100

    feat: add project model and migration

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,4 +8,30 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'user_id',
+        'client_id',
+        'deadline',
+        'status'
+    ];
+
+    public const STATUS = ['open', 'in progress', 'blocked', 'cancelled', 'completed'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
